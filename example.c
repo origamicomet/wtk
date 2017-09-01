987d3ba0da25efdbda50e857b1a876202336dae8
Michael Williams <m.t.williams@live.com>
2017-09-01 11:47:03 -0700

    Use clip stack in example.

--- example.c
+++ example.c
@@ -46,15 +46,18 @@ int main(int argc, const char *argv[]) {
 
   while (1) {
     wtk_canvas_begin(canvas);
+      wtk_canvas_rect(canvas, {0,       0,  1280, 720}, 0x7f7f7fff, 0);
       wtk_canvas_rect(canvas, {0 * 128, 0,   128, 128}, 0xff0000ff, 0);
       wtk_canvas_rect(canvas, {1 * 128, 0,   128, 128}, 0x00ff00ff, 0);
       wtk_canvas_rect(canvas, {2 * 128, 0,   128, 128}, 0x0000ffff, 0);
       wtk_canvas_rect(canvas, {3 * 128, 0,   128, 128}, 0x7f0000ff, 0);
       wtk_canvas_rect(canvas, {4 * 128, 0,   128, 128}, 0x007f00ff, 0);
       wtk_canvas_rect(canvas, {5 * 128, 0,   128, 128}, 0x00007fff, 0);
-      wtk_canvas_rect(canvas, {6 * 128, 0,   128, 128}, 0x3f0000ff, 0);
-      wtk_canvas_rect(canvas, {7 * 128, 0,   128, 128}, 0x003f00ff, 0);
-      wtk_canvas_rect(canvas, {8 * 128, 0,   128, 128}, 0x00003fff, 0);
+      wtk_canvas_push(canvas, {768 + 32, 32, 384 - 32 - 32, 64});
+        wtk_canvas_rect(canvas, {6 * 128, 0,   128, 128}, 0x3f0000ff, 0);
+        wtk_canvas_rect(canvas, {7 * 128, 0,   128, 128}, 0x003f00ff, 0);
+        wtk_canvas_rect(canvas, {8 * 128, 0,   128, 128}, 0x00003fff, 0);
+      wtk_canvas_pop(canvas);
     wtk_canvas_end(canvas);
 
     wtk_app_pump();
