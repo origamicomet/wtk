c6aa36a801b096bef51b575954f865aed225fee6
Michael Williams <m.t.williams@live.com>
2017-09-02 14:37:36 -0700

    Add support macros.

--- include/wtk/foundation/support.h
+++ include/wtk/foundation/support.h
@@ -9,13 +9,59 @@
 /*                                                                            */
 /*===----------------------------------------------------------------------===*/
 
-#ifndef _WTK_FOUNDATION_SUPPORt_H_
-#define _WTK_FOUNDATION_SUPPORt_H_
+#ifndef _WTK_FOUNDATION_SUPPORT_H_
+#define _WTK_FOUNDATION_SUPPORT_H_
 
 #include "wtk/config.h"
 #include "wtk/linkage.h"
 
-WTK_BEGIN_EXTERN_C
-WTK_END_EXTERN_C
+/**
+ * \def WTK_INLINE
+ * \brief Code should be inlined.
+ */
+#if defined(DOXYGEN)
+  #define WTK_INLINE
+#else
+  #if defined(_MSC_VER)
+    #define WTK_INLINE __forceinline
+  #elif defined(__clang__) || defined(__GNUC__)
+    #define WTK_INLINE __inline __attribute__ ((always_inline))
+  #endif
+#endif
 
-#endif /* _WTK_FOUNDATION_SUPPORt_H_ */
+/**
+ * \def WTK_TRAP
+ * \brief Errant, but reachable, code path.
+ */
+#if defined(DOXYGEN)
+  #define WTK_TRAP()
+#else
+  #if defined(_MSC_VER)
+    #define WTK_TRAP() __debugbreak()
+  #elif defined(__GNUC__)
+    #define WTK_TRAP() __builtin_trap()
+  #endif
+#endif
+
+/**
+ * \def WTK_UNREACHABLE
+ * \brief Code is unreachable.
+ */
+#if defined(DOXYGEN)
+  #define WTK_UNREACHABLE()
+#else
+  #if defined(_MSC_VER)
+    #define WTK_UNREACHABLE() __assume(0)
+  #elif defined(__clang__)
+    #define WTK_UNREACHABLE() __builtin_unreachable()
+  #elif defined(__GNUC__)
+    #if __GNUC_VERSION__ >= 40500
+      #define WTK_UNREACHABLE() __builtin_unreachable()
+    #else
+      #include <signal.h>
+      #define WTK_UNREACHABLE() do { ::signal(SIGTRAP); } while(0, 0)
+    #endif
+  #endif
+#endif
+
+#endif /* _WTK_FOUNDATION_SUPPORT_H_ */
